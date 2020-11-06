e5bb1b750b39dcbbc3ed6514b7bbde809ce81e16
felipe89 <gfelipe.trindade@gmail.com>
2020-11-06 10:24:01 -0300

    Configuraçao yml rodando testes em varios ambientes

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,11 +3,16 @@
  require 'site_prism'
  require_relative 'page_helper.rb'
 
+ AMBIENTES = ENV['AMBIENTE']
+
+ CONFIG = YAML.load_file(File.dirname(__FILE__) + "/ambientes/#{AMBIENTES}.yml")
+
  World(PageObjects)
 
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
-  config.app_host = 'https://automacaocombatista.herokuapp.com/'
+  config.app_host = CONFIG['url_padrao'] #Carregar url do yml confirme parametro informado acima
+                    CONFIG['user'] #Carregar user criado dentro do yml
   config.default_max_wait_time = 5
  end
 
